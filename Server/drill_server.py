685b296258856596ae20962b4c0447b0c692c6e0
basharnavaz <reachBashar@live.com>
2017-08-03 16:45:29 -0400

    Replicate GUI to display

--- Server/drill_server.py
+++ Server/drill_server.py
@@ -1,52 +1,163 @@
+# Author: Basharnavaz Khan
+#
+# This code is for live plotting of data being received from Raspberry Pi
+import matplotlib
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.figure import Figure
+import matplotlib.animation as animation
+from matplotlib import style
+import Tkinter as tk
 import socket
-import time
-import pickle
-from Tkinter import *
-import threading
 import numpy as np
+import pickle
+import threading
+# from tkinter import ttk
+
+matplotlib.use("TkAgg")
+LARGE_FONT = ("Verdana", 12)
+style.use("ggplot")
+
+f = Figure(figsize=(5, 5), dpi=100)
+a = f.add_subplot(111)
 
 
-def transmission():
-    global vector
+def sock_receive():
+    global plot_data
     ip = '192.168.1.185'
     port = 5005
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((ip, port))
-    t_0 = time.time()
+    simulation = True
 
-    while time.time() - t_0 < 100:
+    while simulation:
         data, addr = sock.recvfrom(512)
         try:
             vector = pickle.loads(data)
-        except pickle.UnpicklingError:
-            print 'Error: ', data
+        except EOFError or pickle.UnpicklingError:
+            if data == 'Terminate':
+                print 'Terminating'
+                break
+            continue
+        plot = np.array([[vector[6], vector[5]]])
+        plot_data = np.concatenate((plot_data, plot.T), axis=1)
 
 
-def gui_update():
-    global vector
-    label_vals[0]['text'] = str(drill_status_names[int(vector[0])])
-    for k in xrange(1, 7):
-        label_vals[k]['text'] = str(vector[k])[0:5]
-    label_vals[0].after(100, gui_update)
+def animate(i):
+    global plot_data
+    # pullData = open("sampleText.txt", "r").read()
+    # dataList = pullData.split('\n')
+    # xList = []
+    # yList = []
+    # for eachLine in dataList:
+    #     if len(eachLine) > 1:
+    #         x, y = eachLine.split(',')
+    #         xList.append(int(x))
+    #         yList.append(int(y))
 
+    a.clear()
+    a.plot(plot_data[0, :], plot_data[1, :])
+
+
+class DrillGUI(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        tk.Tk.wm_title(self, 'Drill Data')
+
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        for F in (StartPage, PageOne, PageTwo, PageThree):
+            frame = F(container, self)
+
+            self.frames[F] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(StartPage)
+
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+
+class StartPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Start Page", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+
+        button = tk.Button(self, text="Visit Page 1",
+                           command=lambda: controller.show_frame(PageOne))
+        button.pack()
+
+        button2 = tk.Button(self, text="Visit Page 2",
+                            command=lambda: controller.show_frame(PageTwo))
+        button2.pack()
+
+        button3 = tk.Button(self, text="Graph Page",
+                            command=lambda: controller.show_frame(PageThree))
+        button3.pack()
+
+
+class PageOne(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Page One!!!", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+
+        button1 = tk.Button(self, text="Back to Home",
+                            command=lambda: controller.show_frame(StartPage))
+        button1.pack()
+
+        button2 = tk.Button(self, text="Page Two",
+                            command=lambda: controller.show_frame(PageTwo))
+        button2.pack()
+
+
+class PageTwo(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Page Two!!!", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+
+        button1 = tk.Button(self, text="Back to Home",
+                            command=lambda: controller.show_frame(StartPage))
+        button1.pack()
+
+        button2 = tk.Button(self, text="Page One",
+                            command=lambda: controller.show_frame(PageOne))
+        button2.pack()
+
+
+class PageThree(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Graph Page!", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+
+        button1 = tk.Button(self, text="Back to Home",
+                            command=lambda: controller.show_frame(StartPage))
+        button1.pack()
+
+        canvas = FigureCanvasTkAgg(f, self)
+        canvas.show()
+        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+
+        toolbar = NavigationToolbar2TkAgg(canvas, self)
+        toolbar.update()
+        canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
 if __name__ == '__main__':
-    vector = np.zeros(7)
-    receive = threading.Thread(name='Socket', target=transmission)
+    plot_data = np.zeros((2, 1))
+    print 'Plot data shape: ', np.shape(plot_data)
+    receive = threading.Thread(name='Socket', target=sock_receive)
     receive.start()
-
-    drill_status_names = ['Not Started,', 'Moving', 'Tramming', 'Levelling', 'Drilling', 'Clearing']
-    names = ['Drill Status', 'Hole Number', 'X Pos', 'Y Pos', 'Depth', 'Feed Pressure', 'Time Elapsed']
-    root = Tk()
-    label_names = []
-    label_vals = []
-    for i in xrange(7):
-        label_names.append(Label(root, text=names[i], font=20))
-        label_vals.append(Label(root, font=20))
-
-        label_names[i].grid(row=0, column=i)
-        label_vals[i].grid(row=1, column=i)
-
-    gui_update()
-    root.mainloop()
+    app = DrillGUI()
+    ani = animation.FuncAnimation(f, animate, interval=100)
+    app.mainloop()
